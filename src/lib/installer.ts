8df2d1f43edc4625e86b1a81e033c0aa72812284
Ralf Hannuschka <tsmonkeypatch@gmail.com>
2021-02-15 00:30:12 +0100

    support multiple versions
    - add support to download / use multiple versions

--- src/lib/installer.ts
+++ src/lib/installer.ts
@@ -5,111 +5,131 @@ import fetch from 'node-fetch'
 import fs from 'fs'
 import os from 'os'
 import path from 'path'
-import readline from 'readline'
 import * as vscode from 'vscode'
-import { ELECTRON_VERSION, ELECTRON_INSTALL_PATH, OUTPUT_CHANNEL } from './api'
+import { ELECTRON_INSTALL_PATH, OUTPUT_CHANNEL, ELECTRON_ENV_VARS, DATA_NODE } from './api'
 
 @singleton()
 export class ElectronInstaller {
 
-    private isAvailable = false;
+    private _latestVersion: string = "";
+
+    private installedVersions: string[] = [];
+
+    private checkedInstalledVersions = false;
 
     public constructor(
-        @inject(ELECTRON_VERSION) private version: string,
         @inject(ELECTRON_INSTALL_PATH) private installPath: string,
-        @inject(OUTPUT_CHANNEL) private output: vscode.OutputChannel
+        @inject(OUTPUT_CHANNEL) private output: vscode.OutputChannel,
+        @inject(ELECTRON_ENV_VARS) private envVars: DATA_NODE
     ) {}
+
+    /**
+     * get path from our installed electron
+     * btw we could say it is a valid installation if we have an command
+     *
+     */
+    get versions(): Promise<string[]> {
+
+        if (this.checkedInstalledVersions) {
+            return Promise.resolve(this.installedVersions)
+        }
+
+        const global = this.resolveGlobalElectron().then(([version]) => [version]);
+        const local =  new Promise<string[]>((resolve, reject) => {
+            const versionsPath = path.resolve(__dirname, 'versions')
+            fs.readFile(versionsPath, {encoding: 'utf8'}, (err, data) => {
+                err ? reject() : resolve(Object.keys(JSON.parse(data)))
+            })
+        })
+        .catch<string[]>(() => [])
+
+        return Promise.all<string[]>([global, local])
+            .then((data) =>data.reduce((current, next) => current.concat(next),[]))
+            .then((values) => {
+                this.installedVersions = values.filter((value, index, data) => data.indexOf(value) === index)
+                return this.installedVersions
+            })
+    }
+
+    /**
+     * set latest version
+     *
+     */
+    set latestVersion(version: string) {
+        this._latestVersion = version;
+    }
+
+    /**
+     * resolve latest version
+     *
+     */
+    get latestVersion(): string {
+        return this._latestVersion;
+    }
 
     /**
      * install electron
      *
      */
-    public async install(): Promise<void> {
-        const isAvailable = await this.isElectronAvailable()
+    async install(version?: string): Promise<string> {
 
-        this.output.show()
+        const installedVersions: string[] = await this.versions
+        const requiredVersion = version || await this.latestRelease()
+        this.output.appendLine(`required version ${requiredVersion}`)
+        const install = installedVersions.indexOf(requiredVersion) === -1
 
-        if (!isAvailable) {
-            const installDir = path.resolve(__dirname, this.installPath)
-            if (!fs.existsSync(installDir)) {
-                fs.mkdirSync(installDir)
-            }
+        if (install) {
+            this.output.show()
 
-            await this.downloadElectron()
-            await this.extractFile()
-            this.finalizeInstallation()
+            const installDir  = path.resolve(__dirname, this.installPath + '_' + requiredVersion)
+            this.output.appendLine(`install directory ${requiredVersion}`)
+            const downloadUrl = this.resolveDownloadUrl(requiredVersion)
+
+            fs.mkdirSync(installDir)
+
+            await this.downloadElectron(downloadUrl, installDir)
+            await this.extractFile(installDir)
+            this.finalizeInstallation(requiredVersion, installDir)
 
             this.output.appendLine('installation completed')
-            return;
         }
 
-        this.output.appendLine(`electron allready installed`)
+        return requiredVersion
     }
 
     /**
-     * validate electron installation
+     * get latest release
      *
      */
-    public async validateInstallation(): Promise<boolean> {
-        return this.isElectronAvailable();
+    async latestRelease(): Promise<string> {
+        if (this.latestVersion === "") {
+            const response = await fetch('https://api.github.com/repos/electron/electron/releases/latest')
+            const body     = await response.json()
+            this.latestVersion = body.tag_name as string
+        }
+        return this.latestVersion
     }
 
     /**
-     * get current electron version
+     * check version is installed
      *
      */
-    public async readElectronVersion(): Promise<string | null> {
-
-        return new Promise((resolve, reject) => {
-            const versionFile = path.resolve(__dirname, this.installPath, 'version')
-
-            if (!fs.existsSync(versionFile) || fs.statSync(versionFile).isFile) {
-                return null
-            }
-
-            const rs = fs.createReadStream(versionFile, {encoding: 'utf8'})
-            const line = readline.createInterface(rs)
-
-            line.on("line", (version) => resolve(version))
-        })
-    }
-
-    /**
-     * get path from our installed electron
-     *
-     */
-    public resolveElectronCommand(): Promise<string | null> {
-
-        return new Promise((resolve) => {
-            const pathFile = path.resolve(__dirname, 'path')
-
-            if (!fs.existsSync(pathFile) || !fs.statSync(pathFile).isFile) {
-                return resolve(null)
-            }
-
-            const rs = fs.createReadStream(pathFile, {encoding: 'utf8'})
-            const line = readline.createInterface(rs)
-
-            line.on("line", (command) => {
-                line.close()
-                rs.close()
-
-                resolve(command)
-            })
-        })
+    async validate(version?: string): Promise<boolean> {
+        const versions = await this.versions;
+        return versions.indexOf(version ?? this.latestVersion) > -1;
     }
 
     /**
      * download electron binary from github
      *
      */
-    private async downloadElectron(): Promise<void> {
+    private async downloadElectron(url: string, directory: string): Promise<void> {
 
-        this.output.appendLine(`Download from: ${this.resolveDownloadUrl()} ...`)
+        this.output.appendLine(`Download from: ${url} ...`)
 
-        const download    = await fetch(this.resolveDownloadUrl())
+        const download    = await fetch(url)
         const filename    = `electron.zip`
-        const fileStream  = fs.createWriteStream(path.join(__dirname, this.installPath, filename), {flags: 'wx' })
+        const fileStream  = fs.createWriteStream(path.join(directory, filename), {flags: 'wx'})
 
         download.body.pipe(fileStream)
 
@@ -128,47 +148,88 @@ export class ElectronInstaller {
     }
 
     /**
+     * extract elctron binary
+     *
+     */
+    private extractFile (directory: string): Promise<void> {
+        const source = path.resolve(directory, `electron.zip`)
+        const outDir = directory
+
+        this.output.appendLine(`extract file: ${source} to ${outDir}`)
+
+        return new Promise((resolve, reject) => {
+            const childProcess = spawn("tar", ["-zxvf", source, "-C", outDir], {stdio: "ignore", env: this.envVars})
+            childProcess.on("exit", (code) => code === 0 ? resolve() : reject())
+        })
+    }
+
+    /**
      * finalize installation
      * write path.txt and delete electron.zip
      * 
      */
-    public finalizeInstallation(): void {
+    private finalizeInstallation(version: string, directory: string): void {
+        const zipFile = path.join(directory, 'electron.zip')
+        const execPath = path.join(directory, this.resolvePlatformPath())
 
-        const zipFile = path.join(__dirname, this.installPath, 'electron.zip')
-        const execPath = path.join(__dirname, this.installPath, this.resolvePlatformPath())
-
-        this.output.appendLine(`Write executable path: ${execPath}`)
-        this.writeExecutablePath(execPath)
+        this.output.appendLine(`Write to versions: ${execPath}`)
+        this.writeVersion(version, execPath)
+        this.installedVersions.push(version)
 
         this.output.appendLine(`Finalize installation: remove file ${zipFile}`)
         fs.unlinkSync(zipFile)
     }
 
     /**
+     * check electron is installed globaly
+     *
+     */
+    private async resolveGlobalElectron(): Promise<string[]> {
+
+        return this.resolveGlobalElectronVersion()
+            .then((version) => [version, os.platform() === 'win32' ? 'electron.cmd' : 'electron'])
+            .catch(() => {
+                const command = path.resolve('/Applications', this.resolvePlatformPath())
+
+                let performCheck = os.platform() === 'darwin'
+                performCheck = performCheck && fs.existsSync(command) 
+                performCheck = performCheck && fs.statSync(command).isFile()
+
+                if (performCheck) {
+                    return this.resolveGlobalElectronVersion(command)
+                        .then((version) => [version, command])
+                }
+                return []
+            })
+            .then(([version, command]) => {
+                if (!version) {
+                    return [];
+                }
+                this.writeVersion(version, command);
+                return [version, command];
+            })
+    }
+
+    /**
+     * resolve global installed electron version
+     *
+     */
+    private resolveGlobalElectronVersion(command = `electron`): Promise<string> {
+        return new Promise((resolve, reject) => {
+            exec(`${command} --version`, {env: this.envVars}, (err, version) => {
+                err ? reject() : resolve(version.replace(/[\r\n]/, "").trim())
+            })
+        })
+    }
+
+    /**
      * build download url for electron
      * 
      */
-    private resolveDownloadUrl(): string {
-        const baseUrl = `https://github.com/electron/electron/releases/download`
-        const downloadFile = `electron-v${this.version}-${os.platform()}-x64.zip`
-        return baseUrl + '/v' + this.version + '/' + downloadFile
-    }
-    
-    /**
-     * extract elctron binary
-     *
-     */
-    private extractFile (): Promise<void> {
-
-        const source = path.resolve(__dirname, this.installPath, `electron.zip`)
-        const outDir = path.join(__dirname, this.installPath)
-
-        this.output.appendLine(`extract file: ${source} to ${outDir}`)
-
-        return new Promise((resolve, reject) => {
-            const childProcess = spawn("tar", ["-zxvf", source, "-C", outDir], {stdio: "inherit"})
-            childProcess.on("exit", (code) => code === 0 ? resolve() : reject())
-        })
+    private resolveDownloadUrl(version: string): string {
+        const baseUrl = `https://github.com/electron/electron/releases/download/`
+        const downloadFile = `electron-${version}-${os.platform()}-x64.zip`
+        return baseUrl + version + '/' + downloadFile
     }
 
     /**
@@ -193,50 +254,18 @@ export class ElectronInstaller {
     }
 
     /**
-     * check we have an running electron version, first check the path.txt command
-     * if this not exists or is just empty we try global electron installation
-     *
-     */
-    private async isElectronAvailable(): Promise<boolean> {
-
-        if (this.isAvailable) {
-            return true;
-        }
-
-        const command = await this.resolveElectronCommand()
-        const available = await new Promise<boolean>((resolve) => {
-            exec(`${command ?? `electron`} --version`, (err) => {
-                if (!err && !command) {
-                    const command  = os.platform() === 'win32' ? 'electron.cmd' : 'electron'
-                    this.writeExecutablePath(command)
-                }
-                this.isAvailable = err === null
-                resolve(this.isAvailable)
-            })
-        })
-
-        /** only for mac check if not found we can find it in /Applications */
-        if (!available && os.platform() === 'darwin') {
-            const applicationsPath = path.resolve('/Applications', this.resolvePlatformPath())
-
-            if (fs.existsSync(applicationsPath) && fs.statSync(applicationsPath).isFile()) {
-                this.writeExecutablePath(applicationsPath)
-                this.isAvailable = true
-                return this.isAvailable
-            }
-        }
-
-        return available
-    }
-
-    /**
      * write the path to executable binary into the path.txt
      *
      */
-    private writeExecutablePath(location: string) {
-        const pathTxt = path.resolve(__dirname, 'path')
-        const pathTxt$ = fs.createWriteStream(pathTxt)
-        pathTxt$.write(location)
-        pathTxt$.close()
+    private writeVersion(version: string, executablePath: string) {
+        const versionsPath = path.resolve(__dirname, 'versions')
+        let data;
+        try {
+            data = JSON.parse(fs.readFileSync(versionsPath, {encoding: "utf8"}).toString())
+        } catch(error) {
+            data = {}
+        }
+        data[version] = executablePath;
+        fs.writeFileSync(versionsPath, JSON.stringify(data, null, 2));
     }
 }
